071c6ef84eb6f840918e46679e57b4dfeab62681
Jerrorld <jerrorldk@gmail.com>
2022-07-23 14:13:31 -0700

    completed assignment. assets are in place and items are clickable

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(path, left, bottom) {
+    let png = document.createElement('img');
+    png.src = path;
+    png.style.position = 'fixed';
+    png.style.left = left + 'px';
+    png.style.bottom = bottom + 'px';
+    document.body.append(png);
+    return png
+};
+
+function newItem(path, left, bottom) {
+    let object = newImage(path, left, bottom);
+
+    object.addEventListener('dblclick', function(){
+        object.remove();
+    })
+};
+
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
+newImage("assets/pine-tree.png", 450, 200);
+
+newItem('assets/sword.png', 500, 405);
+newItem('assets/shield.png', 165, 185);
+newItem('assets/staff.png', 600, 100);
+
